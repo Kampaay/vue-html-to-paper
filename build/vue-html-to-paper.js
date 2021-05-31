468d33615bc48cdfca25b364c97ee1afaedf6886
Lorenzo Simone <loris32@gmail.com>
2021-05-31 14:49:51 +0200

    fix(build): old build (1.3.1) with new code (1.4.1)

--- build/vue-html-to-paper.js
+++ build/vue-html-to-paper.js
@@ -1,60 +1,70 @@
 (function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-  typeof define === 'function' && define.amd ? define(factory) :
-  (global = global || self, global.VueHtmlToPaper = factory());
+    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+    typeof define === 'function' && define.amd ? define(factory) :
+    (global = global || self, global.VueHtmlToPaper = factory());
 }(this, function () { 'use strict';
 
-  function addStyles (win, styles) {
-    styles.forEach(style => {
-      let link = win.document.createElement('link');
-      link.setAttribute('rel', 'stylesheet');
-      link.setAttribute('type', 'text/css');
-      link.setAttribute('href', style);
-      win.document.getElementsByTagName('head')[0].appendChild(link);
-    });
-  }
+    const addStyles = (win, styles) => {
+        styles.forEach((style) => {
+            let link = win.document.createElement('link');
+            link.setAttribute('rel', 'stylesheet');
+            link.setAttribute('type', 'text/css');
+            link.setAttribute('href', style);
+            win.document.getElementsByTagName('head')[0].appendChild(link);
+        });
+    };
 
-  const VueHtmlToPaper = {
-    install (Vue, options = {}) {
-      Vue.prototype.$htmlToPaper = (el, localOptions, cb = () => true) => {
-        let defaultName = '_blank', 
-            defaultSpecs = ['fullscreen=yes','titlebar=yes', 'scrollbars=yes'],
-            defaultReplace = true,
-            defaultStyles = [];
-        let {
-          name = defaultName,
-          specs = defaultSpecs,
-          replace = defaultReplace,
-          styles = defaultStyles
-        } = options;
+    const VueHtmlToPaper = {
+        install(Vue, options = {}) {
+            Vue.prototype.$htmlToPaper = (
+                el,
+                localOptions = {},
+                cb = () => true
+            ) => {
+                let defaultName = '_blank',
+                    defaultSpecs = [
+                        'fullscreen=yes',
+                        'titlebar=yes',
+                        'scrollbars=yes',
+                    ],
+                    defaultStyles = [],
+                    defaultTimeout = 1000,
+                    defaultAutoClose = true,
+                    defaultWindowTitle = window.document.title;
+                let {
+                    name = defaultName,
+                    specs = defaultSpecs,
+                    styles = defaultStyles,
+                    timeout = defaultTimeout,
+                    autoClose = defaultAutoClose,
+                    windowTitle = defaultWindowTitle,
+                } = options;
 
-        // If has localOptions
-        // TODO: improve logic
-        if (!!localOptions) {
-          if (localOptions.name) name = localOptions.name;
-          if (localOptions.specs) specs = localOptions.specs;
-          if (localOptions.replace) replace = localOptions.replace;
-          if (localOptions.styles) styles = localOptions.styles;
-        }
+                if (localOptions.name) name = localOptions.name;
+                if (localOptions.specs) specs = localOptions.specs;
+                if (localOptions.styles) styles = localOptions.styles;
+                if (localOptions.timeout) timeout = localOptions.timeout;
+                if (localOptions.autoClose) autoClose = localOptions.autoClose;
+                if (localOptions.windowTitle) windowTitle = localOptions.windowTitle;
 
-        console.warn(styles);
+                specs = !!specs.length ? specs.join(',') : '';
 
-        specs = !!specs.length ? specs.join(',') : '';
+                let element = el;
+                if (typeof el === 'string' || el instanceof String) {
+                    element = window.document.getElementById(el);
+                }
+                if (!element) {
+                    alert(`Element to print #${el} not found!`);
+                    return
+                }
 
-        const element = document.getElementById(el);
+                const url = '';
+                const win = window.open(url, name, specs);
 
-        if (!element) {
-          alert(`Element to print #${el} not found!`);
-          return;
-        }
-        
-        const url = '';
-        const win = window.open(url, name, specs, replace);
-
-        win.document.write(`
+                win.document.write(`
         <html>
           <head>
-            <title>${document.title}</title>
+            <title>${windowTitle}</title>
           </head>
           <body>
             ${element.innerHTML}
@@ -62,21 +72,24 @@
         </html>
       `);
 
-        addStyles(win, styles);
-        
-        setTimeout(() => {
-          win.document.close();
-          win.focus();
-          win.print();
-          win.close();
-          cb();
-        }, 1000);
-          
-        return true;
-      };
-    }
-  };
+                addStyles(win, styles);
 
-  return VueHtmlToPaper;
+                const promise = new Promise((resolve) => {
+                    setTimeout(() => {
+                        win.focus();
+                        win.print();
+                        autoClose && win.document.close();
+                        autoClose && win.close();
+                        if (cb) cb();
+                        resolve();
+                    }, timeout);
+                });
+
+                return cb ? true : promise
+            };
+        },
+    };
+
+    return VueHtmlToPaper;
 
 }));
--- src/index.js
+++ src/index.js
@@ -1,72 +1,87 @@
-function addStyles (win, styles) {
-  styles.forEach(style => {
-    let link = win.document.createElement('link');
-    link.setAttribute('rel', 'stylesheet');
-    link.setAttribute('type', 'text/css');
-    link.setAttribute('href', style);
-    win.document.getElementsByTagName('head')[0].appendChild(link);
-  });
+const addStyles = (win, styles) => {
+    styles.forEach((style) => {
+        let link = win.document.createElement('link')
+        link.setAttribute('rel', 'stylesheet')
+        link.setAttribute('type', 'text/css')
+        link.setAttribute('href', style)
+        win.document.getElementsByTagName('head')[0].appendChild(link)
+    })
 }
 
 const VueHtmlToPaper = {
-  install (Vue, options = {}) {
-    Vue.prototype.$htmlToPaper = (el, localOptions, cb = () => true) => {
-      let defaultName = '_blank', 
-          defaultSpecs = ['fullscreen=yes','titlebar=yes', 'scrollbars=yes'],
-          defaultReplace = true,
-          defaultStyles = []
-      let {
-        name = defaultName,
-        specs = defaultSpecs,
-        replace = defaultReplace,
-        styles = defaultStyles
-      } = options;
+    install(Vue, options = {}) {
+        Vue.prototype.$htmlToPaper = (
+            el,
+            localOptions = {},
+            cb = () => true
+        ) => {
+            let defaultName = '_blank',
+                defaultSpecs = [
+                    'fullscreen=yes',
+                    'titlebar=yes',
+                    'scrollbars=yes',
+                ],
+                defaultStyles = [],
+                defaultTimeout = 1000,
+                defaultAutoClose = true,
+                defaultWindowTitle = window.document.title
+            let {
+                name = defaultName,
+                specs = defaultSpecs,
+                styles = defaultStyles,
+                timeout = defaultTimeout,
+                autoClose = defaultAutoClose,
+                windowTitle = defaultWindowTitle,
+            } = options
 
-      // If has localOptions
-      // TODO: improve logic
-      if (!!localOptions) {
-        if (localOptions.name) name = localOptions.name;
-        if (localOptions.specs) specs = localOptions.specs;
-        if (localOptions.replace) replace = localOptions.replace;
-        if (localOptions.styles) styles = localOptions.styles;
-      }
+            if (localOptions.name) name = localOptions.name
+            if (localOptions.specs) specs = localOptions.specs
+            if (localOptions.styles) styles = localOptions.styles
+            if (localOptions.timeout) timeout = localOptions.timeout
+            if (localOptions.autoClose) autoClose = localOptions.autoClose
+            if (localOptions.windowTitle) windowTitle = localOptions.windowTitle
 
-      specs = !!specs.length ? specs.join(',') : '';
+            specs = !!specs.length ? specs.join(',') : ''
 
-      const element = window.document.getElementById(el);
+            let element = el
+            if (typeof el === 'string' || el instanceof String) {
+                element = window.document.getElementById(el)
+            }
+            if (!element) {
+                alert(`Element to print #${el} not found!`)
+                return
+            }
 
-      if (!element) {
-        alert(`Element to print #${el} not found!`);
-        return;
-      }
-      
-      const url = '';
-      const win = window.open(url, name, specs, replace);
+            const url = ''
+            const win = window.open(url, name, specs)
 
-      win.document.write(`
+            win.document.write(`
         <html>
           <head>
-            <title>${window.document.title}</title>
+            <title>${windowTitle}</title>
           </head>
           <body>
             ${element.innerHTML}
           </body>
         </html>
-      `);
+      `)
 
-      addStyles(win, styles);
-      
-      setTimeout(() => {
-        win.document.close();
-        win.focus();
-        win.print();
-        win.close();
-        cb();
-      }, 1000);
-        
-      return true;
-    };
-  }
+            addStyles(win, styles)
+
+            const promise = new Promise((resolve) => {
+                setTimeout(() => {
+                    win.focus()
+                    win.print()
+                    autoClose && win.document.close()
+                    autoClose && win.close()
+                    if (cb) cb()
+                    resolve()
+                }, timeout)
+            })
+
+            return cb ? true : promise
+        }
+    },
 }
 
-export default VueHtmlToPaper;
+export default VueHtmlToPaper
